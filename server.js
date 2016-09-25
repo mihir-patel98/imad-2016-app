b34ec71c80b40cde652c70d704de0568c1235234
mihir-patel98 <150120107101@git.org.in>
2016-09-25 11:17:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-res.send("Article-one is served is here");
+res.sendFile(path.join(__dirname, 'ui', 'fir.html*'));
 });
 
 app.get('/article-two',function(req,res){
